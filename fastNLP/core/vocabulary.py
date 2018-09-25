2698094d8fbed283706a6de851e94499c20352d5
yunfan <yunfan.shao@outlook.com>
2018-09-25 17:31:04 +0800

    update embedding loader & vocab

--- fastNLP/core/vocabulary.py
+++ fastNLP/core/vocabulary.py
@@ -18,6 +18,15 @@ def isiterable(p_object):
         return False
     return True
 
+def check_build_vocab(func):
+    def _wrapper(self, *args, **kwargs):
+        if self.word2idx is None:
+            self.build_vocab()
+            self.build_reverse_vocab()
+        elif self.idx2word is None:
+            self.build_reverse_vocab()
+        return func(self, *args, **kwargs)
+    return _wrapper
 
 class Vocabulary(object):
     """Use for word and index one to one mapping
@@ -30,13 +39,42 @@ class Vocabulary(object):
         vocab["word"]
         vocab.to_word(5)
     """
-
-    def __init__(self, need_default=True):
+    def __init__(self, need_default=True, max_size=None, min_freq=None):
         """
         :param bool need_default: set if the Vocabulary has default labels reserved for sequences. Default: True.
-
+        :param int max_size: set the max number of words in Vocabulary. Default: None
+        :param int min_freq: set the min occur frequency of words in Vocabulary. Default: None
         """
-        if need_default:
+        self.max_size = max_size
+        self.min_freq = min_freq
+        self.word_count = {}
+        self.has_default = need_default
+        self.word2idx = None
+        self.idx2word = None
+
+
+    def update(self, word):
+        """add word or list of words into Vocabulary
+
+        :param word: a list of string or a single string
+        """
+        if not isinstance(word, str) and isiterable(word):
+            # it's a nested list
+            for w in word:
+                self.update(w)
+        else:
+        # it's a word to be added
+            if word not in self.word_count:
+                self.word_count[word] = 1
+            else:
+                self.word_count[word] += 1
+            self.word2idx = None
+
+
+    def build_vocab(self):
+        """build 'word to index' dict, and filter the word using `max_size` and `min_freq`
+        """
+        if self.has_default:
             self.word2idx = deepcopy(DEFAULT_WORD_TO_INDEX)
             self.padding_label = DEFAULT_PADDING_LABEL
             self.unknown_label = DEFAULT_UNKNOWN_LABEL
@@ -45,28 +83,28 @@ class Vocabulary(object):
             self.padding_label = None
             self.unknown_label = None
 
-        self.has_default = need_default
-        self.idx2word = None
+        words = sorted(self.word_count.items(), key=lambda kv: kv[1], reverse=True)
+        if self.min_freq is not None:
+            words = list(filter(lambda kv: kv[1] >= self.min_freq, words))
+        if self.max_size is not None and len(words) > self.max_size:
+            words = words[:self.max_size]
+        for w, _ in words:
+            self.word2idx[w] = len(self.word2idx)
 
+    def build_reverse_vocab(self):
+        """build 'index to word' dict based on 'word to index' dict
+        """
+        self.idx2word = {self.word2idx[w] : w for w in self.word2idx}
+
+    @check_build_vocab
     def __len__(self):
         return len(self.word2idx)
 
-    def update(self, word):
-        """add word or list of words into Vocabulary
-        
-        :param word: a list of string or a single string
-        """
-        if not isinstance(word, str) and isiterable(word):
-            # it's a nested list
-            for w in word:
-                self.update(w)
-        else:
-            # it's a word to be added
-            if word not in self.word2idx:
-                self.word2idx[word] = len(self)
-                if self.idx2word is not None:
-                    self.idx2word = None
+    @check_build_vocab
+    def has_word(self, w):
+        return w in self.word2idx
 
+    @check_build_vocab
     def __getitem__(self, w):
         """To support usage like::
 
@@ -74,32 +112,33 @@ class Vocabulary(object):
         """
         if w in self.word2idx:
             return self.word2idx[w]
-        else:
+        elif self.has_default:
             return self.word2idx[DEFAULT_UNKNOWN_LABEL]
+        else:
+            raise ValueError("word {} not in vocabulary".format(w))
 
+    @check_build_vocab
     def to_index(self, w):
         """ like to_index(w) function, turn a word to the index
             if w is not in Vocabulary, return the unknown label
-        
+
         :param str w:
         """
         return self[w]
 
+    @check_build_vocab
     def unknown_idx(self):
         if self.unknown_label is None:
             return None
         return self.word2idx[self.unknown_label]
 
+    @check_build_vocab
     def padding_idx(self):
         if self.padding_label is None:
             return None
         return self.word2idx[self.padding_label]
 
-    def build_reverse_vocab(self):
-        """build 'index to word' dict based on 'word to index' dict
-        """
-        self.idx2word = {self.word2idx[w]: w for w in self.word2idx}
-
+    @check_build_vocab
     def to_word(self, idx):
         """given a word's index, return the word itself
 
