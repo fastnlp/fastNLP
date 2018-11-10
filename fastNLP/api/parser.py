1806bbdbec72ebc926348bc70ae98739b699fbf2
yunfan <yunfan.shao@outlook.com>
2018-11-10 15:13:53 +0800

    fix dataset

--- fastNLP/api/parser.py
+++ fastNLP/api/parser.py
@@ -3,6 +3,7 @@ from fastNLP.core.dataset import DataSet
 from fastNLP.core.predictor import Predictor
 from fastNLP.api.pipeline import Pipeline
 from fastNLP.api.processor import *
+from fastNLP.models.biaffine_parser import BiaffineParser
 
 
 class DependencyParser(API):
@@ -23,9 +24,13 @@ class DependencyParser(API):
     def build(self):
         pipe = Pipeline()
 
+        # build pipeline
         word_seq = 'word_seq'
         pos_seq = 'pos_seq'
-        pipe.add_processor(Num2TagProcessor('<NUM>', word_seq))
+        pipe.add_processor(Num2TagProcessor('<NUM>', 'raw_sentence', word_seq))
         pipe.add_processor(IndexerProcessor(word_vocab, word_seq, word_seq+'_idx'))
         pipe.add_processor(IndexerProcessor(pos_vocab, pos_seq, pos_seq+'_idx'))
-        pipe.add_processor()
+
+        # load model parameters
+        self.model = BiaffineParser()
+        self.pipeline = pipe
