8906155ca2e86f16868d683d27d5caa4234a653a
yh <poemsmileyh@gmail.com>
2018-11-14 23:15:19 +0800

    为api建立一个Analyzer

--- fastNLP/api/api.py
+++ fastNLP/api/api.py
@@ -34,7 +34,6 @@ class API:
         if os.path.exists(os.path.expanduser(path)):
             _dict = torch.load(path, map_location='cpu')
         else:
-            print(os.path.expanduser(path))
             _dict = load_url(path, map_location='cpu')
         self.pipeline = _dict['pipeline']
         self._dict = _dict
@@ -58,7 +57,7 @@ class POS(API):
     def predict(self, content):
         """
 
-        :param query: list of list of str. Each string is a token(word).
+        :param content: list of list of str. Each string is a token(word).
         :return answer: list of list of str. Each string is a tag.
         """
         if not hasattr(self, 'pipeline'):
@@ -183,99 +182,64 @@ class CWS(API):
         return f1, pre, rec
 
 
-class Parser(API):
-    def __init__(self, model_path=None, device='cpu'):
-        super(Parser, self).__init__()
-        if model_path is None:
-            model_path = model_urls['parser']
+class Analyzer:
+    def __init__(self, seg=True, pos=True, parser=True, device='cpu'):
 
-        self.load(model_path, device)
+        self.seg = seg
+        self.pos = pos
+        self.parser = parser
+
+        if self.seg:
+            self.cws = CWS(device=device)
+        if self.pos:
+            self.pos = POS(device=device)
+        if parser:
+            self.parser = None
 
     def predict(self, content):
-        if not hasattr(self, 'pipeline'):
-            raise ValueError("You have to load model first.")
+        output_dict = {}
+        if self.seg:
+            seg_output = self.cws.predict(content)
+            output_dict['seg'] = seg_output
+        if self.pos:
+            pos_output = self.pos.predict(content)
+            output_dict['pos'] = pos_output
+        if self.parser:
+            parser_output = self.parser.predict(content)
+            output_dict['parser'] = parser_output
 
-        sentence_list = []
-        # 1. 检查sentence的类型
-        if isinstance(content, str):
-            sentence_list.append(content)
-        elif isinstance(content, list):
-            sentence_list = content
-
-        # 2. 组建dataset
-        dataset = DataSet()
-        dataset.add_field('words', sentence_list)
-        # dataset.add_field('tag', sentence_list)
-
-        # 3. 使用pipeline
-        self.pipeline(dataset)
-        for ins in dataset:
-            ins['heads'] = ins['heads'].tolist()
-
-        return dataset['heads'], dataset['labels']
+        return output_dict
 
     def test(self, filepath):
-        data = ConllxDataLoader().load(filepath)
-        ds = DataSet()
-        for ins1, ins2 in zip(add_seg_tag(data), data):
-            ds.append(Instance(words=ins1[0], tag=ins1[1],
-                               gold_words=ins2[0], gold_pos=ins2[1],
-                               gold_heads=ins2[2], gold_head_tags=ins2[3]))
+        output_dict = {}
+        if self.seg:
+            seg_output = self.cws.test(filepath)
+            output_dict['seg'] = seg_output
+        if self.pos:
+            pos_output = self.pos.test(filepath)
+            output_dict['pos'] = pos_output
+        if self.parser:
+            parser_output = self.parser.test(filepath)
+            output_dict['parser'] = parser_output
 
-        pp = self.pipeline
-        for p in pp:
-            if p.field_name == 'word_list':
-                p.field_name = 'gold_words'
-            elif p.field_name == 'pos_list':
-                p.field_name = 'gold_pos'
-        pp(ds)
-        head_cor, label_cor, total = 0, 0, 0
-        for ins in ds:
-            head_gold = ins['gold_heads']
-            head_pred = ins['heads']
-            length = len(head_gold)
-            total += length
-            for i in range(length):
-                head_cor += 1 if head_pred[i] == head_gold[i] else 0
-        uas = head_cor / total
-        print('uas:{:.2f}'.format(uas))
-
-        for p in pp:
-            if p.field_name == 'gold_words':
-                p.field_name = 'word_list'
-            elif p.field_name == 'gold_pos':
-                p.field_name = 'pos_list'
-
-        return uas
+        return output_dict
 
 
 if __name__ == "__main__":
-    # 以下路径在102
-    """
-    pos_model_path = '/home/hyan/fastNLP_models/upload-demo/upload/pos_crf-5e26d3b0.pkl'
-    pos = POS(model_path=pos_model_path, device='cpu')
-    s = ['编者按：7月12日，英国航空航天系统公司公布了该公司研制的第一款高科技隐形无人机雷电之神。',
-         '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
-         '那么这款无人机到底有多厉害？']
-    #print(pos.test('../../reproduction/chinese_word_segment/new-clean.txt.conll'))
-    print(pos.predict(s))
-    """
+    # pos_model_path = '../../reproduction/pos_tag_model/pos_crf.pkl'
+    # pos = POS(device='cpu')
+    # s = ['编者按：7月12日，英国航空航天系统公司公布了该公司研制的第一款高科技隐形无人机雷电之神。' ,
+    #     '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
+    #      '那么这款无人机到底有多厉害？']
+    # print(pos.test('/Users/yh/Desktop/test_data/small_test.conll'))
+    # print(pos.predict(s))
 
-    """
-    cws_model_path = '/home/hyan/fastNLP_models/upload-demo/upload/cws_crf-5a8a3e66.pkl'
-    cws = CWS(model_path=cws_model_path, device='cuda:0')
-    s = ['本品是一个抗酸抗胆汁的胃黏膜保护剂',
-         '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
+    # cws_model_path = '../../reproduction/chinese_word_segment/models/cws_crf.pkl'
+    cws = CWS(device='cpu')
+    s = ['本品是一个抗酸抗胆汁的胃黏膜保护剂' ,
+        '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
          '那么这款无人机到底有多厉害？']
-    #print(cws.test('../../reproduction/chinese_word_segment/new-clean.txt.conll'))
-    cws.predict(s)
-    """
+    print(cws.test('/Users/yh/Desktop/test_data/small_test.conll'))
+    print(cws.predict(s))
 
-    parser_model_path = "/home/hyan/fastNLP_models/upload-demo/upload/parser-d57cd5fc.pkl"
-    parser = Parser(model_path=parser_model_path, device='cuda:0')
-    # print(parser.test('../../reproduction/Biaffine_parser/test.conll'))
-    s = ['编者按：7月12日，英国航空航天系统公司公布了该公司研制的第一款高科技隐形无人机雷电之神。',
-         '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
-         '那么这款无人机到底有多厉害？']
-    print(parser.predict(s))
 
