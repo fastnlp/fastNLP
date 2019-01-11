7eb02f17621af790a6afb4bf0f94a7452923206f
FengZiYjun <writerphone@163.com>
2019-01-11 20:59:42 +0800

    update code comments in CRF

--- fastNLP/modules/aggregator/attention.py
+++ fastNLP/modules/aggregator/attention.py
@@ -1,7 +1,9 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
 import math
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
 from fastNLP.modules.utils import mask_softmax
 
 
