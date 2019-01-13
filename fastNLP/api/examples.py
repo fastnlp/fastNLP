094a566155a9084c7fd7302a53656d1b9b250dd6
FengZiYjun <writerphone@163.com>
2019-01-13 11:49:38 +0800

    add syntactic parsing API

--- fastNLP/api/examples.py
+++ fastNLP/api/examples.py
@@ -3,7 +3,7 @@ api/example.py contains all API examples provided by fastNLP.
 It is used as a tutorial for API or a test script since it is difficult to test APIs in travis.
 
 """
-from fastNLP.api import CWS, POS
+from fastNLP.api import CWS, POS, Parser
 
 text = ['编者按：7月12日，英国航空航天系统公司公布了该公司研制的第一款高科技隐形无人机雷电之神。',
         '这款飞行从外型上来看酷似电影中的太空飞行器，据英国方面介绍，可以实现洲际远程打击。',
@@ -20,5 +20,10 @@ def pos_tagging():
     print(pos.predict(text))
 
 
+def syntactic_parsing():
+    parser = Parser(device='cpu')
+    print(parser.predict(text))
+
+
 if __name__ == "__main__":
-    pos_tagging()
+    syntactic_parsing()
