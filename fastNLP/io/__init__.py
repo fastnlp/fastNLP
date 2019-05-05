f466c502c5abae393bd4631c48b46cbcac1133f6
ChenXin <will131@foxmail.com>
2019-05-05 22:27:38 +0800

    修改文档中的引用部分

--- fastNLP/io/__init__.py
+++ fastNLP/io/__init__.py
@@ -1,13 +1,13 @@
 """
 用于IO的模块, 具体包括:
 
-1. 用于读入 embedding 的 :ref:`EmbedLoader <embed-loader>` 类,
+1. 用于读入 embedding 的 :doc:`EmbedLoader <fastNLP.io.embed_loader>` 类,
 
-2. 用于读入数据的 :ref:`DataSetLoader <dataset-loader>` 类
+2. 用于读入数据的 :doc:`DataSetLoader <fastNLP.io.dataset_loader>` 类
 
-3. 用于读写config文件的类, 参考 :ref:`Config-io <config-io>`
+3. 用于读写config文件的类, 参考 :doc:`Config-IO <fastNLP.io.config_io>`
 
-4. 用于保存和载入模型的类, 参考 :ref:`Model-io <model-io>`
+4. 用于保存和载入模型的类, 参考 :doc:`Model-IO <fastNLP.io.model_io>`
 
 这些类的使用方法可以在对应module的文档下查看.
 """
