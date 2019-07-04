4b5713b5a2e2e2dcedf2b757659295016db6a9ce
yunfan <yunfan.shao@outlook.com>
2019-07-04 14:03:53 +0800

    update model & dataloader in text_classification

--- reproduction/text_classification/data/yelpLoader.py
+++ reproduction/text_classification/data/yelpLoader.py
@@ -1,4 +1,6 @@
 import ast
+import csv
+from typing import Iterable
 from fastNLP import DataSet, Instance, Vocabulary
 from fastNLP.core.vocabulary import VocabularyOption
 from fastNLP.io import JsonLoader
@@ -10,11 +12,34 @@ from reproduction.Star_transformer.datasets import EmbedLoader
 from reproduction.utils import check_dataloader_paths
 
 
+def clean_str(sentence, char_lower=False):
+    """
+    heavily borrowed from github
+    https://github.com/LukeZhuang/Hierarchical-Attention-Network/blob/master/yelp-preprocess.ipynb
+    :param sentence:  is a str
+    :return:
+    """
+    if char_lower:
+        sentence = sentence.lower()
+    import re
+    nonalpnum = re.compile('[^0-9a-zA-Z?!\']+')
+    words = sentence.split()
+    words_collection = []
+    for word in words:
+        if word in ['-lrb-', '-rrb-', '<sssss>', '-r', '-l', 'b-']:
+            continue
+        tt = nonalpnum.split(word)
+        t = ''.join(tt)
+        if t != '':
+            words_collection.append(t)
+
+    return words_collection
+
+
 class yelpLoader(JsonLoader):
-    
     """
     读取Yelp数据集, DataSet包含fields:
-    
+
         review_id: str, 22 character unique review id
         user_id: str, 22 character unique user id
         business_id: str, 22 character business id
@@ -24,23 +49,25 @@ class yelpLoader(JsonLoader):
         date: str, date formatted YYYY-MM-DD
         words: list(str), 需要分类的文本
         target: str, 文本的标签
-    
+
     数据来源: https://www.yelp.com/dataset/download
-    
+
     :param fine_grained: 是否使用SST-5标准，若 ``False`` , 使用SST-2。Default: ``False``
     """
-    
-    def __init__(self, fine_grained=False):
+
+    def __init__(self, fine_grained=False, lower=False):
         super(yelpLoader, self).__init__()
         tag_v = {'1.0': 'very negative', '2.0': 'negative', '3.0': 'neutral',
-            '4.0': 'positive', '5.0': 'very positive'}
+                 '4.0': 'positive', '5.0': 'very positive'}
         if not fine_grained:
             tag_v['1.0'] = tag_v['2.0']
             tag_v['5.0'] = tag_v['4.0']
         self.fine_grained = fine_grained
         self.tag_v = tag_v
-    
-    def _load(self, path):
+        self.lower = lower
+
+    '''
+    def _load_json(self, path):
         ds = DataSet()
         for idx, d in _read_json(path, fields=self.fields_list, dropna=self.dropna):
             d = ast.literal_eval(d)
@@ -49,20 +76,113 @@ class yelpLoader(JsonLoader):
             ds.append(Instance(**d))
         return ds
 
-    def process(self, paths: Union[str, Dict[str, str]], vocab_opt: VocabularyOption = None,
-                embed_opt: EmbeddingOption = None):
+    def _load_yelp2015_broken(self,path):
+        ds = DataSet()
+        with open (path,encoding='ISO 8859-1') as f:
+            row=f.readline()
+            all_count=0
+            exp_count=0
+            while row:
+                row=row.split("\t\t")
+                all_count+=1
+                if len(row)>=3:
+                    words=row[-1].split()
+                    try:
+                        target=self.tag_v[str(row[-2])+".0"]
+                        ds.append(Instance(words=words, target=target))
+                    except KeyError:
+                        exp_count+=1
+                else:
+                    exp_count+=1
+                row = f.readline()
+            print("error sample count:",exp_count)
+            print("all count:",all_count)
+        return ds
+    '''
+
+    def _load(self, path):
+        ds = DataSet()
+        csv_reader = csv.reader(open(path, encoding='utf-8'))
+        all_count = 0
+        real_count = 0
+        for row in csv_reader:
+            all_count += 1
+            if len(row) == 2:
+                target = self.tag_v[row[0] + ".0"]
+                words = clean_str(row[1], self.lower)
+                if len(words) != 0:
+                    ds.append(Instance(words=words, target=target))
+                    real_count += 1
+        print("all count:", all_count)
+        print("real count:", real_count)
+        return ds
+
+    def process(self, paths: Union[str, Dict[str, str]],
+                train_ds: Iterable[str] = None,
+                src_vocab_op: VocabularyOption = None,
+                tgt_vocab_op: VocabularyOption = None,
+                embed_opt: EmbeddingOption = None,
+                char_level_op=False):
         paths = check_dataloader_paths(paths)
         datasets = {}
-        info = DataInfo()
-        vocab = Vocabulary(min_freq=2) if vocab_opt is None else Vocabulary(**vocab_opt)
-        for name, path in paths.items():
-            dataset = self.load(path)
-            datasets[name] = dataset
-            vocab.from_dataset(dataset, field_name="words")
-        info.vocabs = vocab
-        info.datasets = datasets
-        if embed_opt is not None:
-            embed = EmbedLoader.load_with_vocab(**embed_opt, vocab=vocab)
-            info.embeddings['words'] = embed
+        info = DataInfo(datasets=self.load(paths))
+        src_vocab = Vocabulary() if src_vocab_op is None else Vocabulary(**src_vocab_op)
+        tgt_vocab = Vocabulary(unknown=None, padding=None) \
+            if tgt_vocab_op is None else Vocabulary(**tgt_vocab_op)
+        _train_ds = [info.datasets[name]
+                     for name in train_ds] if train_ds else info.datasets.values()
+
+        # vocab = Vocabulary(min_freq=2) if vocab_opt is None else Vocabulary(**vocab_opt)
+        # for name, path in paths.items():
+        #     dataset = self.load(path)
+        #     datasets[name] = dataset
+        #     vocab.from_dataset(dataset, field_name="words")
+        # info.vocabs = vocab
+        # info.datasets = datasets
+
+        def wordtochar(words):
+            chars = []
+            for word in words:
+                word = word.lower()
+                for char in word:
+                    chars.append(char)
+            return chars
+
+        input_name, target_name = 'words', 'target'
+        info.vocabs = {}
+        # 就分隔为char形式
+        if char_level_op:
+            for dataset in info.datasets.values():
+                dataset.apply_field(wordtochar, field_name="words", new_field_name='chars')
+        # if embed_opt is not None:
+        #     embed = EmbedLoader.load_with_vocab(**embed_opt, vocab=vocab)
+        #     info.embeddings['words'] = embed
+        else:
+            src_vocab.from_dataset(*_train_ds, field_name=input_name)
+            src_vocab.index_dataset(*info.datasets.values(), field_name=input_name, new_field_name=input_name)
+            info.vocabs[input_name] = src_vocab
+
+        tgt_vocab.from_dataset(*_train_ds, field_name=target_name)
+        tgt_vocab.index_dataset(
+            *info.datasets.values(),
+            field_name=target_name, new_field_name=target_name)
+        info.vocabs[target_name] = tgt_vocab
+
         return info
 
+
+if __name__ == "__main__":
+    testloader = yelpLoader()
+    # datapath = {"train": "/remote-home/ygwang/yelp_full/train.csv",
+    #             "test": "/remote-home/ygwang/yelp_full/test.csv"}
+    # datapath={"train": "/remote-home/ygwang/yelp_full/test.csv"}
+    datapath = {"train": "/remote-home/ygwang/yelp_polarity/train.csv",
+                "test": "/remote-home/ygwang/yelp_polarity/test.csv"}
+    datainfo = testloader.process(datapath, char_level_op=True)
+
+    len_count = 0
+    for instance in datainfo.datasets["train"]:
+        len_count += len(instance["chars"])
+
+    ave_len = len_count / len(datainfo.datasets["train"])
+    print(ave_len)
