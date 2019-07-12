90c6454aed18343c8cf2e03d5a75c3fc364ad80c
ChenXin <will131@foxmail.com>
2019-07-12 13:13:54 +0800

    修改了文档首页

--- fastNLP/modules/__init__.py
+++ fastNLP/modules/__init__.py
@@ -9,7 +9,7 @@
 .. csv-table::
    :header: "类型", "功能", "常见组件"
 
-   "embedding", 参见 :doc:`fastNLP.embeddings` ,  "Elmo, Bert"
+   "embedding", 参见 :doc:`/fastNLP.embeddings` ,  "Elmo, Bert"
    "encoder", "将输入编码为具有表示能力的向量", "CNN, LSTM, Transformer"
    "decoder", "将具有某种表示意义的向量解码为需要的输出形式 ", "MLP, CRF"
    "其它", "配合其它组件使用的组件", "Dropout"
