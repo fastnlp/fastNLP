a2e31584883abb68e4d7354ca0c95fc250e35605
ChenXin <will131@foxmail.com>
2019-09-04 15:50:01 +0800

    update the auto alias tool

--- fastNLP/doc_utils.py
+++ fastNLP/doc_utils.py
@@ -13,7 +13,8 @@ def doc_process(m):
                     while 1:
                         defined_m = sys.modules[module_name]
                         if "undocumented" not in defined_m.__doc__ and name in defined_m.__all__:
-                            obj.__doc__ = r"定义在 :class:`" + module_name + "." + name + "`\n" + obj.__doc__
+                            obj.__doc__ = r"别名 :class:`" + m.__name__ + "." + name + "`" \
+                                          + " :class:`" + module_name + "." + name + "`\n" + obj.__doc__
                             break
                         module_name = ".".join(module_name.split('.')[:-1])
                         if module_name == m.__name__:
