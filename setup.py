c421a6d7bbf22af6b767353669346aefe16541f9
yh_cc <poemsmileyh@gmail.com>
2021-10-15 11:12:55 +0800

    开始v0.7.1的开发

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ print(pkgs)
 
 setup(
     name='FastNLP',
-    version='0.6.0',
+    version='0.7.1',
     url='https://gitee.com/fastnlp/fastNLP',
     description='fastNLP: Deep Learning Toolkit for NLP, developed by Fudan FastNLP Team',
     long_description=readme,
