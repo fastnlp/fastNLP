c7616f8ba8d62100077eda7971297679796b9407
MorningForest <2297662686@qq.com>
2022-04-12 13:51:54 +0800

    修改ClassifyF1PreRecMetric及对应的测试用例

--- fastNLP/core/metrics/classify_f1_pre_rec_metric.py
+++ fastNLP/core/metrics/classify_f1_pre_rec_metric.py
@@ -29,14 +29,16 @@ def _compute_f_pre_rec(beta_square, tp, fn, fp):
 
 
 class ClassifyFPreRecMetric(Metric):
-    def __init__(self, backend: Union[str, Backend, None] = 'auto', aggregate_when_get_metric: bool = False,
-                 tag_vocab: Vocabulary = None, encoding_type: str = None, ignore_labels: List[str] = None,
-                 only_gross: bool = True, f_type='micro', beta=1) -> None:
+    def __init__(self, tag_vocab: Vocabulary = None, ignore_labels: List[str] = None, num_class: int = 0,
+                 only_gross: bool = True, f_type='micro', beta=1, backend: Union[str, Backend, None] = 'auto',
+                 aggregate_when_get_metric: bool = False) -> None:
         super(ClassifyFPreRecMetric, self).__init__(backend=backend,
                                                     aggregate_when_get_metric=aggregate_when_get_metric)
         if f_type not in ('micro', 'macro'):
             raise ValueError("f_type only supports `micro` or `macro`', got {}.".format(f_type))
-
+        if tag_vocab:
+            if not isinstance(tag_vocab, Vocabulary):
+                raise TypeError("tag_vocab can only be fastNLP.Vocabulary, not {}.".format(type(tag_vocab)))
         self.ignore_labels = ignore_labels
         self.f_type = f_type
         self.beta = beta
@@ -45,9 +47,32 @@ class ClassifyFPreRecMetric(Metric):
 
         self.tag_vocab = tag_vocab
 
-        self._tp, self._fp, self._fn = defaultdict(partial(self.register_element, aggregate_method='sum')),\
-                                       defaultdict(partial(self.register_element, aggregate_method='sum')),\
-                                       defaultdict(partial(self.register_element, aggregate_method='sum'))
+        self._tp = {}
+        self._fp = {}
+        self._fn = {}
+        if tag_vocab:
+            for word, _ in tag_vocab:
+                word = word.lower()
+                if word != 'o':
+                    word = word[2:]
+                if word in self._true_positives:
+                    continue
+                self._tp[word] = self.register_element(name=f'tp_{word}', aggregate_method='sum',
+                                                       backend=backend)
+                self._fn[word] = self.register_element(name=f'fn_{word}', aggregate_method='sum',
+                                                       backend=backend)
+                self._fp[word] = self.register_element(name=f'fp_{word}', aggregate_method='sum',
+                                                       backend=backend)
+        elif num_class > 0:
+            for word in range(num_class):
+                self._tp[word] = self.register_element(name=f'tp_{word}', aggregate_method='sum',
+                                                       backend=backend)
+                self._fn[word] = self.register_element(name=f'fn_{word}', aggregate_method='sum',
+                                                       backend=backend)
+                self._fp[word] = self.register_element(name=f'fp_{word}', aggregate_method='sum',
+                                                       backend=backend)
+        else:
+            raise ValueError()
 
     def get_metric(self) -> dict:
         r"""
@@ -68,9 +93,11 @@ class ClassifyFPreRecMetric(Metric):
                     tag_name = self.tag_vocab.to_word(tag)
                 else:
                     tag_name = int(tag)
-                tp = self._tp[tag]
-                fn = self._fn[tag]
-                fp = self._fp[tag]
+                tp = self._tp[tag].get_scalar()
+                fn = self._fn[tag].get_scalar()
+                fp = self._fp[tag].get_scalar()
+                if tp == fn == fp == 0:
+                    continue
                 f, pre, rec = _compute_f_pre_rec(self.beta_square, tp, fn, fp)
                 f_sum += f
                 pre_sum += pre
@@ -90,13 +117,12 @@ class ClassifyFPreRecMetric(Metric):
 
         if self.f_type == 'micro':
             f, pre, rec = _compute_f_pre_rec(self.beta_square,
-                                             sum(self._tp.values()),
-                                             sum(self._fn.values()),
-                                             sum(self._fp.values()))
+                                             sum(val.get_scalar() for val in self._tp.values()),
+                                             sum(val.get_scalar() for val in self._fn.values()),
+                                             sum(val.get_scalar() for val in self._fp.values()))
             evaluate_result['f'] = f
             evaluate_result['pre'] = pre
             evaluate_result['rec'] = rec
-
 
         for key, value in evaluate_result.items():
             evaluate_result[key] = round(value, 6)
@@ -104,6 +130,16 @@ class ClassifyFPreRecMetric(Metric):
         return evaluate_result
 
     def update(self, pred, target, seq_len=None):
+        r"""
+        evaluate函数将针对一个批次的预测结果做评价指标的累计
+
+        :param torch.Tensor pred: 预测的tensor, tensor的形状可以是torch.Size([B,]), torch.Size([B, n_classes]),
+                torch.Size([B, max_len]), 或者torch.Size([B, max_len, n_classes])
+        :param torch.Tensor target: 真实值的tensor, tensor的形状可以是Element's can be: torch.Size([B,]),
+                torch.Size([B,]), torch.Size([B, max_len]), 或者torch.Size([B, max_len])
+        :param torch.Tensor seq_len: 序列长度标记, 标记的形状可以是None, None, torch.Size([B]), 或者torch.Size([B]).
+                如果mask也被传进来的话seq_len会被忽略.
+        """
         pred = self.tensor2numpy(pred)
         target = self.tensor2numpy(target)
         if seq_len is not None:
@@ -122,14 +158,14 @@ class ClassifyFPreRecMetric(Metric):
                                    f"pred have element numbers: {len(target.flatten())}")
 
             pass
-        elif len(pred.ndim) == len(target.ndim) + 1:
+        elif pred.ndim == target.ndim + 1:
             pred = pred.argmax(axis=-1)
-            if seq_len is None and len(target.ndim) > 1:
+            if seq_len is None and target.ndim > 1:
                 warnings.warn("You are not passing `seq_len` to exclude pad when calculate accuracy.")
         else:
             raise RuntimeError(f"when pred have "
-                               f"size:{pred.ndim}, target should have size: {pred.ndim} or "
-                               f"{pred.ndim[:-1]}, got {target.ndim}.")
+                               f"size:{pred.shape}, target should have size: {pred.shape} or "
+                               f"{pred.shape[:-1]}, got {target.shape}.")
         if masks is not None:
             target = target * masks
             pred = pred * masks
@@ -138,5 +174,3 @@ class ClassifyFPreRecMetric(Metric):
             self._tp[target_idx] += ((pred == target_idx) * (target != target_idx)).sum().item()
             self._fp[target_idx] += ((pred == target_idx) * (target == target_idx)).sum().item()
             self._fn[target_idx] += ((pred != target_idx) * (target != target_idx)).sum().item()
-
-
