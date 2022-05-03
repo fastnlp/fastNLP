80ef85cf7c5533b3bacba11a1052896bb9228d35
x54-729 <17307130121@fudan.edu.cn>
2022-05-03 14:38:35 +0000

    为 paddle 分布式的测试例添加paddledist标签

--- tests/core/controllers/test_trainer_paddle.py
+++ tests/core/controllers/test_trainer_paddle.py
@@ -27,7 +27,7 @@ class TrainPaddleConfig:
 @pytest.mark.parametrize("driver,device", [("paddle", "cpu"), ("paddle", 1), ("fleet", [0, 1])])
 # @pytest.mark.parametrize("driver,device", [("fleet", [0, 1])])
 @pytest.mark.parametrize("callbacks", [[RichCallback(5)]])
-@pytest.mark.paddle
+@pytest.mark.paddledist
 @magic_argv_env_context
 def test_trainer_paddle(
         driver,
