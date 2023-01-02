f61b074918d7bbb20e03e879921d85fca0e2c2cb
x54-729 <17307130121@fudan.edu.cn>
2023-01-02 21:56:07 +0800

    修改部分标签

--- tests/core/metrics/test_bleu_other_backend.py
+++ tests/core/metrics/test_bleu_other_backend.py
@@ -2,8 +2,9 @@ import os
 
 import pytest
 
-from fastNLP import DataSet, Bleu
-from fastNLP.envs.imports import _NEED_IMPORT_PADDLE, _NEED_IMPORT_ONEFLOW, _NEED_IMPORT_JITTOR
+from fastNLP import Bleu, DataSet
+from fastNLP.envs.imports import (_NEED_IMPORT_JITTOR, _NEED_IMPORT_ONEFLOW,
+                                  _NEED_IMPORT_PADDLE)
 
 if _NEED_IMPORT_PADDLE:
     import paddle
@@ -18,87 +19,99 @@ if _NEED_IMPORT_JITTOR:
 @pytest.mark.paddle
 def test_bleu_paddle():
     dataset = DataSet({
-        "predictions": ['the cat is on the mat', 'There is a big tree near the park here',
-                        'The sun rises from the northeast with sunshine', 'I was late for work today for the rainy'],
-        "references": [['a cat is on the mat'], ['A big tree is growing near the park here'],
-                       ["A fierce sun rises in the northeast with sunshine"],
+        'predictions': [
+            'the cat is on the mat', 'There is a big tree near the park here',
+            'The sun rises from the northeast with sunshine',
+            'I was late for work today for the rainy'
+        ],
+        'references': [['a cat is on the mat'],
+                       ['A big tree is growing near the park here'],
+                       ['A fierce sun rises in the northeast with sunshine'],
                        ['I went to work too late today for the rainy']]
     })
-    metric = Bleu(backend="paddle")
+    metric = Bleu(backend='paddle')
     for i in range(0, len(dataset), 2):
-        prediction1 = dataset[i]["predictions"]
-        prediction2 = dataset[i + 1]["predictions"]
-        references1 = dataset[i]["references"]
-        references2 = dataset[i + 1]["references"]
+        prediction1 = dataset[i]['predictions']
+        prediction2 = dataset[i + 1]['predictions']
+        references1 = dataset[i]['references']
+        references2 = dataset[i + 1]['references']
         metric.update([prediction1, prediction2], [references1, references2])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.4181
 
-    metric = Bleu(backend="paddle", smooth=True)
+    metric = Bleu(backend='paddle', smooth=True)
     for i in range(0, len(dataset), 2):
-        prediction1 = dataset[i]["predictions"]
-        prediction2 = dataset[i + 1]["predictions"]
-        references1 = dataset[i]["references"]
-        references2 = dataset[i + 1]["references"]
+        prediction1 = dataset[i]['predictions']
+        prediction2 = dataset[i + 1]['predictions']
+        references1 = dataset[i]['references']
+        references2 = dataset[i + 1]['references']
         metric.update([prediction1, prediction2], [references1, references2])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.442571
 
 
 @pytest.mark.oneflow
 def test_bleu_oneflow():
     dataset = DataSet({
-        "predictions": ['the cat is on the mat', 'There is a big tree near the park here',
-                        'The sun rises from the northeast with sunshine', 'I was late for work today for the rainy'],
-        "references": [['a cat is on the mat'], ['A big tree is growing near the park here'],
-                       ["A fierce sun rises in the northeast with sunshine"],
+        'predictions': [
+            'the cat is on the mat', 'There is a big tree near the park here',
+            'The sun rises from the northeast with sunshine',
+            'I was late for work today for the rainy'
+        ],
+        'references': [['a cat is on the mat'],
+                       ['A big tree is growing near the park here'],
+                       ['A fierce sun rises in the northeast with sunshine'],
                        ['I went to work too late today for the rainy']]
     })
-    metric = Bleu(backend="oneflow")
+    metric = Bleu(backend='oneflow')
     for i in range(len(dataset)):
-        predictions = dataset[i]["predictions"]
-        references = dataset[i]["references"]
+        predictions = dataset[i]['predictions']
+        references = dataset[i]['references']
         metric.update([predictions], [references])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.4181
 
-    metric = Bleu(backend="oneflow", smooth=True)
+    metric = Bleu(backend='oneflow', smooth=True)
     for i in range(len(dataset)):
-        predictions = dataset[i]["predictions"]
-        references = dataset[i]["references"]
+        predictions = dataset[i]['predictions']
+        references = dataset[i]['references']
         metric.update([predictions], [references])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.442571
 
 
-@pytest.mark.oneflow
+@pytest.mark.jittor
 def test_bleu_jittor():
     dataset = DataSet({
-        "predictions": ['the cat is on the mat', 'There is a big tree near the park here',
-                        'The sun rises from the northeast with sunshine', 'I was late for work today for the rainy'],
-        "references": [['a cat is on the mat'], ['A big tree is growing near the park here'],
-                       ["A fierce sun rises in the northeast with sunshine"],
+        'predictions': [
+            'the cat is on the mat', 'There is a big tree near the park here',
+            'The sun rises from the northeast with sunshine',
+            'I was late for work today for the rainy'
+        ],
+        'references': [['a cat is on the mat'],
+                       ['A big tree is growing near the park here'],
+                       ['A fierce sun rises in the northeast with sunshine'],
                        ['I went to work too late today for the rainy']]
     })
-    metric = Bleu(backend="jittor")
+    metric = Bleu(backend='jittor')
 
     for i in range(len(dataset)):
-        predictions = dataset[i]["predictions"]
-        references = dataset[i]["references"]
+        predictions = dataset[i]['predictions']
+        references = dataset[i]['references']
         metric.update([predictions], [references])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.4181
 
-    metric = Bleu(backend="jittor", smooth=True)
+    metric = Bleu(backend='jittor', smooth=True)
     for i in range(len(dataset)):
-        predictions = dataset[i]["predictions"]
-        references = dataset[i]["references"]
+        predictions = dataset[i]['predictions']
+        references = dataset[i]['references']
         metric.update([predictions], [references])
     my_result = metric.get_metric()
-    result = my_result["bleu"]
+    result = my_result['bleu']
     assert result == 0.442571
